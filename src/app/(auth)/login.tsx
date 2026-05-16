de830386d12354f741acb3966d82e92d92f3d236
Sheriff Ali <elemlak202099@gmail.com>
2026-05-16 14:19:20 +0300

    Remove Google login code from login screen

--- src/app/(auth)/login.tsx
+++ src/app/(auth)/login.tsx
@@ -1,11 +1,6 @@
-import { env } from "@/src/config/env";
-import { api } from "@/src/services/api";
 import { useAuthStore } from "@/src/stores/auth-store";
-import * as Google from "expo-auth-session/providers/google";
-import { router } from "expo-router";
 import * as WebBrowser from "expo-web-browser";
 import { Button, Text } from "heroui-native";
-import { useEffect } from "react";
 import { Image, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
@@ -14,27 +9,6 @@ WebBrowser.maybeCompleteAuthSession();
 export default function Login() {
   const insets = useSafeAreaInsets();
   const setSession = useAuthStore((s) => s.setSession);
-  const [request, response, promptAsync] = Google.useAuthRequest({
-    webClientId: env.googleWebClientId,
-    androidClientId: env.googleAndroidClientId,
-  });
-
-  useEffect(() => {
-    const login = async () => {
-      if (response?.type !== "success") return;
-
-      const idToken = response.authentication?.idToken;
-
-      if (!idToken) return;
-
-      const res = await api.post("/auth/goole", { idToken });
-
-      await setSession(res.data.accessToken, res.data.refreshToken);
-
-      router.replace("/(app)/home");
-    };
-    login();
-  }, [response]);
 
   return (
     <View
@@ -50,8 +24,7 @@ export default function Login() {
         resizeMode="cover"
       />
       <Button
-        isDisabled={!request}
-        onPress={() => promptAsync()}
+        onPress={() => {}}
         className="w-full mt-24"
         size="md"
         variant="tertiary"
